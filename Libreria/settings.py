abc4e1f1be86c6d94804c881fecd46167c45717f
vecarope <vecarope@mac-vcaro.local>
2023-04-24 10:23:57 -0400

    models and database

--- Libreria/settings.py
+++ Libreria/settings.py
@@ -31,6 +31,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'app',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -75,8 +76,12 @@ WSGI_APPLICATION = 'Libreria.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'libreria_db',
+        'USER': 'postgres',
+        'PASSWORD': '1234',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
@@ -103,7 +108,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/4.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
--- app/models.py
+++ app/models.py
@@ -1,3 +1,24 @@
 from django.db import models
 
-# Create your models here.
+# modelo Autor 
+class Autor(models.Model):
+      nombre= models.CharField(max_length=100)
+      
+      def __str__(self):
+        return self.nombre
+      
+#modelo Editorial
+class Editorial(models.Model):
+      nombre =models.CharField(max_length=100)
+      
+      def __str__(self):
+        return self.nombre
+      
+#modelos Libro
+class Libro(models.Model):
+      titulo = models.CharField(max_length=100)
+      autor = models.ForeignKey(Autor, on_delete=models.CASCADE)      
+      editorial = models.ForeignKey(Editorial, on_delete=models.CASCADE)
+      
+      def __str__(self):
+        return self.titulo
