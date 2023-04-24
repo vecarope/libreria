1c9c546e39f84e671e3210cc27f5f90eb5062617
vecarope <vecarope@mac-vcaro.local>
2023-04-24 11:14:05 -0400

    add admin and add bookss in db

--- app/admin.py
+++ app/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import Libro, Autor, Editorial
 
-# Register your models here.
+admin.site.register(Libro)
+admin.site.register(Autor)
+admin.site.register(Editorial)
+
